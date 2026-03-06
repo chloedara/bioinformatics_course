faddde4fe8c53e522afd40cc0937784b9fbb57f9
Chloe Odusola <chloeodusola@Chloes-MacBook-Air.local>
2026-03-06 12:00:53 +0000

    not tested: updated script to make empty dirs and README.md

--- scripts/my-script.sh
+++ scripts/my-script.sh
@@ -1,1 +1,8 @@
 #!/usr/bin/env bash
+mkdir -p analysis docs data
+touch ${analysis}/README.md
+echo "# ${analysis}" >> ${analysis}/README.md
+touch ${docs}/README.md
+echo "# ${docs}" >> ${docs}/README.md
+touch ${data}/README.md
+echo "# ${data}" >> ${data}/README.md
